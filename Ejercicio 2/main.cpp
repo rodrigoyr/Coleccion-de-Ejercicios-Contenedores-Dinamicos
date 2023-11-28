6dd97c2fee948ca2ce6b10080bcf70f4ea680e91
rodrigoyr <ryeperub@myuax.com>
2023-11-28 10:49:23 +0100

    better code commit

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -6,7 +6,7 @@ int main() {
     std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
     std::stack<float, std::list<float>> pila(lista);
 
-    while(!pila.empty()) {
+    while (!pila.empty()) {
         std::cout << pila.top() << std::endl;
         pila.pop();
     }

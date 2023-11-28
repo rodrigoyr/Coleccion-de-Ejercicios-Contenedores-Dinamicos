3716e42ae117351fda91497b2b9e44c9881c943a
rodrigoyr <ryeperub@myuax.com>
2023-11-28 10:54:33 +0100

    code commit

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <valarray>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::valarray<double> valores = {1.0, 2.0, 3.0, 4.0, 5.0};
+
+    std::cout << "Suma: " << valores.sum() << std::endl;
+    std::cout << "Media: " << valores.sum() / valores.size() << std::endl;
+
+    valores += 5.0;
+
+    std::cout << "Valores + 5.0: ";
+    for(double valor : valores) {
+        std::cout << valor << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }

485a2a37be7c5bca2667c1a8777a90ad694077a7
rodrigoyr <ryeperub@myuax.com>
2023-11-28 12:26:45 +0100

    Fixing code commit

--- Ejercicio 6/main.cpp
+++ Ejercicio 6/main.cpp
@@ -1,21 +1,8 @@
 #include <vector>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include "variant.hpp"
-
-int main() {
-    // Tu implementación aquí
-}
-
-// Path: variant.hpp
-#include <vector>
-#include <iostream>
-#include <sstream>
 #include <string>
 #include <map>
-#include "json11.hpp" // Asegúrate de tener esta biblioteca en tu sistema
-#include "jsonlib.hpp" // No estoy seguro de qué biblioteca es esta, asegúrate de tenerla en tu sistema
+#include "json11.hpp"
+#include "jsonlib.hpp"
 
 enum variant_type { Symbol, Number, List, Proc, Lambda, Cadena };
 
@@ -23,7 +10,7 @@ struct Entorno;
 
 class Variant {
 public:
-    typedef Variant(*proc_type) ( const std::vector<Variant>& );
+    typedef Variant(*proc_type)(const std::vector<Variant>&);
     typedef std::vector<Variant>::const_iterator iter;
     typedef std::map<std::string, Variant> map;
 
@@ -33,28 +20,24 @@ public:
     proc_type proc;
     Entorno* env;
 
-    Variant(variant_type type = Symbol) : type(type) , env(0), proc(0) { }
-    Variant(variant_type type, const std::string& val) : type(type), val(val) , env(0) , proc(0) { }
-    Variant(proc_type proc) : type(Proc), proc(proc) , env(0) { }
+    Variant(variant_type type = Symbol) : type(type), env(0), proc(0) {}
+    Variant(variant_type type, const std::string& val) : type(type), val(val), env(0), proc(0) {}
+    Variant(proc_type proc) : type(Proc), proc(proc), env(0) {}
 
     std::string to_string();
     std::string to_json_string();
-    static Variant from_json_string(std::string json);
-    static Variant parse_json(jsonlib::Json job);  // Asegúrate de que esta es la definición correcta de la función
+    static Variant from_json_string(const std::string& json);
+    static Variant parse_json(jsonlib::Json job);
 };
 
 std::string Variant::to_string() {
-    // Tu implementación aquí
 }
 
 std::string Variant::to_json_string() {
-    // Tu implementación aquí
 }
 
-Variant Variant::from_json_string(std::string sjson) {
-    // Tu implementación aquí
+Variant Variant::from_json_string(const std::string& sjson) {
 }
 
 Variant Variant::parse_json(jsonlib::Json job) {
-    // Tu implementación aquí
 }

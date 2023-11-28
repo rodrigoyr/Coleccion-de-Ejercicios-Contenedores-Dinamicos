9c07a6602914b053203aaeb000a2b826a73e59fa
rodrigoyr <ryeperub@myuax.com>
2023-11-28 20:30:37 +0100

    boost library commit

---
+++ Ejercicio 6/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+
+int main() {
+    std::string s = "12345";
+
+    try {
+        int i = boost::lexical_cast<int>(s);
+        std::cout << "Número: " << i << std::endl;
+    } catch(boost::bad_lexical_cast &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}

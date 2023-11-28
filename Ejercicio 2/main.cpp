d66f3b3dd05d4f988606bed2c4607fc49ca7653d
rodrigoyr <ryeperub@myuax.com>
2023-11-28 10:48:27 +0100

    code commit

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <list>
+#include <stack>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::stack<float, std::list<float>> pila(lista);
+
+    while(!pila.empty()) {
+        std::cout << pila.top() << std::endl;
+        pila.pop();
+    }
+
     return 0;
 }

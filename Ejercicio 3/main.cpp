d0cf2c0a0c67ed3762d48b05c01cde767ea7ea00
rodrigoyr <ryeperub@myuax.com>
2023-11-28 10:52:47 +0100

    NEW code commit

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -4,14 +4,17 @@
 
 int main() {
     std::vector<int> numeros = {50, 20, 60, 10, 30, 40};
+
     std::sort(numeros.begin(), numeros.end());
 
-    for(int num : numeros) {
+    std::cout << "Números ordenados:" << std::endl;
+    for (const int& num : numeros) {
         std::cout << num << std::endl;
     }
 
     int maxNum = *std::max_element(numeros.begin(), numeros.end());
-    std::cout << "Numero maximo: " << maxNum << std::endl;
+
+    std::cout << "Número máximo: " << maxNum << std::endl;
 
     return 0;
 }

87f403661fd396e1932195301f6c473b6ac0daf1
rodrigoyr <ryeperub@myuax.com>
2023-11-30 11:15:59 +0100

    final code commit

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -5,11 +5,14 @@
 int main() {
     std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
     std::stack<float, std::list<float>> pila(lista);
+    std::cout << "Operaciones en la pila:" << std::endl;
+    std::cout << "Top de la pila: " << pila.top() << std::endl;
 
-    while (!pila.empty()) {
-        std::cout << pila.top() << std::endl;
-        pila.pop();
-    }
+    pila.push(6.6);
+    std::cout << "Elemento agregado a la pila: " << pila.top() << std::endl;
+
+    pila.pop();
+    std::cout << "Elemento quitado de la pila. Nuevo top: " << pila.top() << std::endl;
 
     return 0;
 }

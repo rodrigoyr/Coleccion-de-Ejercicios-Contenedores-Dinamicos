30acb806c606e0cd1a9a2f0556f4a763b795d3dc
rodrigoyr <ryeperub@myuax.com>
2023-11-30 11:19:13 +0100

    vallaray code commit

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -4,16 +4,37 @@
 int main() {
     std::valarray<double> valores = {1.0, 2.0, 3.0, 4.0, 5.0};
 
-    double suma = valores.sum();
-    double media = suma / valores.size();
+    std::cout << "Valores originales: ";
+    for(double valor : valores) {
+        std::cout << valor << " ";
+    }
+    std::cout << std::endl;
 
-    std::cout << "Suma: " << suma << std::endl;
-    std::cout << "Media: " << media << std::endl;
-
-    valores += 5.0;
+    std::valarray<double> sumaValores = valores + 5.0;
+    std::valarray<double> restaValores = valores - 2.0;
+    std::valarray<double> multiplicacionValores = valores * 3.0;
+    std::valarray<double> divisionValores = valores / 2.0;
 
     std::cout << "Valores + 5.0: ";
-    for (double valor : valores) {
+    for(double valor : sumaValores) {
+        std::cout << valor << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Valores - 2.0: ";
+    for(double valor : restaValores) {
+        std::cout << valor << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Valores * 3.0: ";
+    for(double valor : multiplicacionValores) {
+        std::cout << valor << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Valores / 2.0: ";
+    for(double valor : divisionValores) {
         std::cout << valor << " ";
     }
     std::cout << std::endl;

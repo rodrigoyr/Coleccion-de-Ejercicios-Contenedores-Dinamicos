3cb07c4494f4b1944478d0ac0b04d296df61bed5
rodrigoyr <ryeperub@myuax.com>
2023-11-30 11:14:14 +0100

    fixed push back new code commit

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -2,10 +2,18 @@
 #include <vector>
 
 int main() {
-    std::vector<int> numeros = {10, 20, 30, 40, 50};
+    std::vector<int> numeros;
 
-    for (const auto &numero : numeros) {
-        std::cout << numero << std::endl;
+    numeros.push_back(10);
+    numeros.push_back(20);
+    numeros.push_back(30);
+    numeros.push_back(40);
+    numeros.push_back(50);
+
+    std::cout << "Elementos del vector:" << std::endl;
+
+    for (std::vector<int>::iterator it = numeros.begin(); it != numeros.end(); ++it) {
+        std::cout << *it << std::endl;
     }
 
     return 0;
